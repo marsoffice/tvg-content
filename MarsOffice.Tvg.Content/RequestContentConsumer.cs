2b44e6963831b5b38e1ad9788d7323f2a9c72ce6
Alin Florin <alin@qoffice.ro>
2022-02-17 18:27:01 +0200

    g

--- MarsOffice.Tvg.Content/RequestContentConsumer.cs
+++ MarsOffice.Tvg.Content/RequestContentConsumer.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using MarsOffice.Tvg.Content.Abstractions;
 using MarsOffice.Tvg.Content.Services;
 using Microsoft.Azure.Cosmos.Table;
+using Microsoft.Azure.Storage.Queue.Protocol;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
@@ -20,11 +21,17 @@ namespace MarsOffice.Tvg.Content
 
         [FunctionName("RequestContentConsumer")]
         public async Task Run(
-            [QueueTrigger("request-content", Connection = "localsaconnectionstring")] RequestContent request,
+            [QueueTrigger("request-content", Connection = "localsaconnectionstring")] QueueMessage message,
             [Queue("content-response", Connection = "localsaconnectionstring")] IAsyncCollector<ContentResponse> contentResponseQueue,
             [Table("UsedPosts", Connection = "localsaconnectionstring")] CloudTable usedPostsTable,
             ILogger log)
         {
+            var request = Newtonsoft.Json.JsonConvert.DeserializeObject<RequestContent>(message.Text,
+                    new Newtonsoft.Json.JsonSerializerSettings
+                    {
+                        ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver(),
+                        NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore
+                    });
             try
             {
                 var service = ContentServiceFactory.Create(request.ContentType, _httpClient, usedPostsTable);
@@ -47,15 +54,18 @@ namespace MarsOffice.Tvg.Content
             }
             catch (Exception e)
             {
-                log.LogError(e, "Function threw an exception");
-                await contentResponseQueue.AddAsync(new ContentResponse
+                if (message.DequeueCount >= 5)
                 {
-                    Success = false,
-                    Error = e.Message,
-                    JobId = request.JobId,
-                    VideoId = request.VideoId
-                });
-                await contentResponseQueue.FlushAsync();
+                    log.LogError(e, "Function threw an exception");
+                    await contentResponseQueue.AddAsync(new ContentResponse
+                    {
+                        Success = false,
+                        Error = e.Message,
+                        JobId = request.JobId,
+                        VideoId = request.VideoId
+                    });
+                    await contentResponseQueue.FlushAsync();
+                }
                 throw;
             }
         }
